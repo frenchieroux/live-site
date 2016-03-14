dce395db5520814e296bcedeb428d21cb2e2e6f7
thibault roux <thibault.a.roux@gmail.com>
2016-03-13 20:18:53 -0500

    march2016

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+var static = require('node-static');
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public');
+
+var port = process.env.PORT || 8080;
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(port);
+
+console.log("http server listening on %d", port)
